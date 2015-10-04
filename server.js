cfa0a89f6d22315d64054737fdfb189e37cc54a8
Paul Jones <shmiko@gmail.com>
2015-10-04 19:49:34 +1100

    routes.

--- server.js
+++ server.js
@@ -1,5 +1,59 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
+
+var showSchema = new mongoose.Schema({
+    _id: Number,
+    name: String,
+    airsDayOfWeek: String,
+    airsTime: String,
+    firstAired: Date,
+    genre: [String],
+    network: String,
+    overview: String,
+    rating: Number,
+    ratingCount: Number,
+    status: String,
+    poster: String,
+    subscribers: [{
+        type: mongoose.Schema.Types.ObjectId, ref: 'User'
+    }],
+    episodes: [{
+        season: Number,
+        episodeNumber: Number,
+        episodeName: String,
+        firstAired: Date,
+        overview: String
+    }]
+});
+
+var userSchema = new mongoose.Schema({
+    email: { type: String, unique: true },
+    password: String
+});
+
+userSchema.pre('save', function(next) {
+    var user = this;
+    if (!user.isModified('password')) return next();
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+var User = mongoose.model('User', userSchema);
+var Show = mongoose.model('Show', showSchema);
+mongoose.connect('localhost');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -14,6 +68,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/shows', function(req, res, next) {
+    var query = Show.find();
+    if (req.query.genre) {
+        query.where({ genre: req.query.genre });
+    } else if (req.query.alphabet) {
+        query.where({ name: new RegExp('^' + '[' + req.query.alphabet + ']', 'i') });
+    } else {
+        query.limit(12);
+    }
+    query.exec(function(err, shows) {
+        if (err) return next(err);
+        res.send(shows);
+    });
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
